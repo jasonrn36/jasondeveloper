1801e27b1ea9ca966ff12a8a0e2725e709cdaec6
jsonrn36 <jason.rn36@gmail.com>
2025-11-06 14:23:21 -0400

    criando um portal site de comunicação

--- server/server.ts
+++ server/server.ts
@@ -3,9 +3,4 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import postRoutes from './routes/posts'; // rotas das postagens
 
-// Area das constantes
-const app = express();
-app.use(cors());
-app.use(express.json());
-
 
