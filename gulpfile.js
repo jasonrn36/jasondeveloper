40206d9bfb227ec30e125df727d37e8225f4cae1
jsonrn36 <jason.rn36@gmail.com>
2025-11-25 14:17:46 -0400

    configurei o scss e as cores

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,35 @@
-const gulp = require("gulp");
-const sass = require("gulp-sass")(require("sass"));
+import gulp from "gulp";
+import * as sass from "sass";
+import gulpSass from "gulp-sass";
+import imagemin from "gulp-imagemin";
+import imageminMozjpeg from "imagemin-mozjpeg";
+import imageminOptipng from "imagemin-optipng";
+import imageminSvgo from "imagemin-svgo";
 
+const scss = gulpSass(sass);
+
+// Compilar Sass
 function Styles() {
-    return gulp.src("src/Styles/**/*.scss"),
-    gulp.dest("./dist/Styles"),
-    gulp.src("src/Styles/**/*.scss")
-        .pipe(sass().on("error", sass.logError))
-        .pipe(gulp.dest("./dist"));
+  return gulp.src("src/Styles/**/*.scss")
+    .pipe(scss().on("error", scss.logError))
+    .pipe(gulp.dest("./dist/Styles"));
 }
 
-function Watch() {
-    gulp.watch("src/Styles/**/*.scss", Styles);
+// Otimizar imagens
+function Images() {
+  return gulp.src("src/img/**/*")
+    .pipe(imagemin([
+      imageminMozjpeg({ quality: 75, progressive: true }),
+      imageminOptipng({ optimizationLevel: 5 }),
+      imageminSvgo()
+    ]))
+    .pipe(gulp.dest("./dist/img"));
 }
-exports.default = gulp.series(Styles);
-exports.watch = gulp.series(Styles, Watch);
+
+// Observar mudanças
+function Watch() {
+  gulp.watch("src/Styles/**/*.scss", Styles);
+  gulp.watch("src/img/**/*", Images);
+}
+
+export default gulp.series(Styles, Images, Watch);
